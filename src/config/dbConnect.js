94fce6a3ed33a3c48af2b0d361f7208bf5e8b799
Marcos Sanhudo <marcos.sanhudo@gmail.com>
2024-07-07 22:15:50 -0300

    Corrige vazamento de senha.

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://marcos:marcos@learning-environment.dqqvcyi.mongodb.net/livraria?retryWrites=true&w=majority&appName=learning-environment");
+    mongoose.connect("mongodb+srv://marcos:<password>@learning-environment.dqqvcyi.mongodb.net/livraria?retryWrites=true&w=majority&appName=learning-environment");
     return mongoose.connection;
 };
 
