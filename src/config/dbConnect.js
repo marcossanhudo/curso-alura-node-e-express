1107b008d94828bb5bfee22c9a14b027eb1b5d64
Marcos Sanhudo <marcos.sanhudo@gmail.com>
2024-07-07 21:52:27 -0300

    Adiciona o arquivo de configuração ao repositório.

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,3 +1,3 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://marcos:marcos@learning-environment.dqqvcyi.mongodb.net/?retryWrites=true&w=majority&appName=learning-environment");
+mongoose.connect("mongodb+srv://marcos:<password>@learning-environment.dqqvcyi.mongodb.net/?retryWrites=true&w=majority&appName=learning-environment");
