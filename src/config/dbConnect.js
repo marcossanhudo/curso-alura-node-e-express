ca4893a938c392db69fb019b859c584d1eb2323e
Marcos Sanhudo <marcos.sanhudo@gmail.com>
2024-07-07 21:48:08 -0300

    Commita progresso atual.

---
+++ src/config/dbConnect.js
@@ -0,0 +1,3 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://marcos:marcos@learning-environment.dqqvcyi.mongodb.net/?retryWrites=true&w=majority&appName=learning-environment");
