2dfcb552a9d068c73086c0e334dc105b69259f81
Marcos Sanhudo <marcos.sanhudo@gmail.com>
2024-07-07 22:12:04 -0300

    Configura conexão com banco de dados.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (error) => {
+    console.error("Erro de conexão", error);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão ao banco feita com sucesso.")
+});
 
 const app = express();
 app.use(express.json());
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,3 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://marcos:<password>@learning-environment.dqqvcyi.mongodb.net/?retryWrites=true&w=majority&appName=learning-environment");
+async function conectaNaDatabase() {
+    mongoose.connect("mongodb+srv://marcos:<password>@learning-environment.dqqvcyi.mongodb.net/livraria?retryWrites=true&w=majority&appName=learning-environment");
+    return mongoose.connection;
+};
+
+export default conectaNaDatabase;
